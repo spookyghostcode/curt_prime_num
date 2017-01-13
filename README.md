87433e0b25976b57ddd46c8b8f8a753706011aca
happycylinder <aphexpusherofcanada@gmail.com>
2017-01-13 11:40:18 -0600

    Initial python backend set-up

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # curt_prime_num
+
+This is a web application that will generate for the user a variable number of prime numbers, starting with 2. 
---
+++ prime.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/primes', methods=["GET"])
+def primes():
+    
+    if not request.args.get('max'):
+        resp = jsonify({"Error": "Argument 'max' not provided'"})
+        resp.status_code = 400
+        return resp
+
+    try:
+        max_num = int(request.args.get('max'))
+    except ValueError:
+        resp = jsonify({"Error": "'max' must be an integer"})
+        resp.status_code = 400
+        return resp
+
+    prime_list = []
+
+    for i in range(2, (max_num+1)):
+        for j in range(2, i):
+            if i != j:
+                if (i % j) == 0:
+                    break
+        else:
+            prime_list.append(i)
+
+    resp = jsonify({"Success": prime_list})
+    return resp
+
+if __name__ == "__main__":
+    app.run(port=8000)
